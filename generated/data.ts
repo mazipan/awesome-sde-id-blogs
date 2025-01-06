68b1bcd1c91de95649dd5dcfdafef3b882d5bbb1
Irfan Maulana <mazipanneh@gmail.com>
2025-01-06 11:38:11 +0700

    CI: Separate TS file

--- generated/data.ts
+++ generated/data.ts
@@ -1,4 +1,32 @@
-export const awesomeSdeBlogs = {
+
+type BlogItem = {
+  name: string
+  link: string
+}
+
+type Owner = {
+  name: string
+  link: string
+  avatar: string
+}
+
+type DataItem = {
+  blog: BlogItem
+  owner: Owner
+}
+
+type Metadata = {
+  total: number
+  updatedAt: string
+  source: string
+}
+
+export type AwesomeSdeBlogs = {
+  data: DataItem[]
+  metadata: Metadata
+};
+
+export const awesomeSdeBlogs: AwesomeSdeBlogs = {
   "data": [
     {
       "blog": {
--- parser.js
+++ parser.js
@@ -1,26 +1,23 @@
-import {
-  readFileSync,
-  writeFileSync,
-} from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 
 const REGEX = /^\+ \[(.+)\]\((http.+)\)/gi;
 const REGEX_NAME = /\[.+\]/;
 const REGEX_LINK = /\(http.+\)/;
-const REGEX_REPLACER = new RegExp('\\[|\\]|\\(|\\)', 'gi')
+const REGEX_REPLACER = new RegExp('\\[|\\]|\\(|\\)', 'gi');
 
-function getTypeOfLink (link) {
-  if (link.includes('github.com')){
-    return 'github'
+function getTypeOfLink(link) {
+  if (link.includes('github.com')) {
+    return 'github';
   } else if (link.includes('x.com')) {
-    return 'x'
+    return 'x';
   }
 
-  return 'unknown'
+  return 'unknown';
 }
 
-function extractUsername (link) {
-  const allLinksPart = link.split('/')
-  return allLinksPart[allLinksPart.length - 1]
+function extractUsername(link) {
+  const allLinksPart = link.split('/');
+  return allLinksPart[allLinksPart.length - 1];
 }
 
 async function parse() {
@@ -30,19 +27,19 @@ async function parse() {
   for (let line of fileContent.split('\n')) {
     const match = line.match(REGEX);
     if (match) {
-      const markdown = match[0].replace('+ ', '')
+      const markdown = match[0].replace('+ ', '');
       const splitBy = markdown.split('by');
 
-      const blogMarkdown = splitBy[0].trim()
-      const matchBlogName = blogMarkdown.match(REGEX_NAME)
-      const matchBlogLink = blogMarkdown.match(REGEX_LINK)
+      const blogMarkdown = splitBy[0].trim();
+      const matchBlogName = blogMarkdown.match(REGEX_NAME);
+      const matchBlogLink = blogMarkdown.match(REGEX_LINK);
 
-      const ownerMarkdown = splitBy[1].trim()
-      const matchOwnerName = ownerMarkdown.match(REGEX_NAME)
-      const matchOwnerLink = ownerMarkdown.match(REGEX_LINK)
+      const ownerMarkdown = splitBy[1].trim();
+      const matchOwnerName = ownerMarkdown.match(REGEX_NAME);
+      const matchOwnerLink = ownerMarkdown.match(REGEX_LINK);
       const link = matchOwnerLink[0].replace(REGEX_REPLACER, '');
-      const typeOfLink = getTypeOfLink(link)
-      const uname = extractUsername(link)
+      const typeOfLink = getTypeOfLink(link);
+      const uname = extractUsername(link);
 
       result.push({
         blog: {
@@ -52,8 +49,8 @@ async function parse() {
         owner: {
           name: matchOwnerName[0].replace(REGEX_REPLACER, ''),
           link: link,
-          avatar: `https://unavatar.io/${typeOfLink}/${uname}`
-        }
+          avatar: `https://unavatar.io/${typeOfLink}/${uname}`,
+        },
       });
     }
   }
@@ -64,15 +61,51 @@ async function parse() {
       total: result.length,
       updatedAt: new Date().toISOString(),
       source: 'https://github.com/mazipan/awesome-sde-id-blogs',
-    }
-  }
+    },
+  };
 
   writeFileSync('./generated/data.json', JSON.stringify(json, false, 2));
-  writeFileSync('./generated/data.js', `
-/**
- * @type {import("./types").AwesomeSdeBlogs}
- */
-export const awesomeSdeBlogs = ${JSON.stringify(json, false, 2)}`);
+  writeFileSync(
+    './generated/data.js',
+    `export const awesomeSdeBlogs = ${JSON.stringify(json, false, 2)}`
+  );
+
+  writeFileSync(
+    './generated/data.ts',
+    `
+type BlogItem = {
+  name: string
+  link: string
+}
+
+type Owner = {
+  name: string
+  link: string
+  avatar: string
+}
+
+type DataItem = {
+  blog: BlogItem
+  owner: Owner
+}
+
+type Metadata = {
+  total: number
+  updatedAt: string
+  source: string
+}
+
+export type AwesomeSdeBlogs = {
+  data: DataItem[]
+  metadata: Metadata
+};
+
+export const awesomeSdeBlogs: AwesomeSdeBlogs = ${JSON.stringify(
+      json,
+      false,
+      2
+    )}`
+  );
 }
 
 // IIFE
